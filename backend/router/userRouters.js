deb10a1b41a6d36c3482e0bd2c3c759009dcff68
othersidedrl <darelpandeyis@gmail.com>
2025-06-07 17:52:33 +0700

    refactor: improve code formatting and consistency in authRouters.js and userRouters.js

--- backend/router/userRouters.js
+++ backend/router/userRouters.js
@@ -1,38 +1,38 @@
-const userService = require("../services/userService")
+const userService = require("../services/userService");
 
 exports.plugin = {
-    name: "userRoutes",
-    register: async function (server, options) {
-        server.route([
-            {
-                method: "GET",
-                path: "/profile",
-                handler: userService.getUserProfile
-            },
-            {
-                method: "PUT",
-                path: "/profile",
-                handler:  userService.updateUserProfile
-            },
-            {
-                method: "PUT",
-                path: "/profile/email",
-                handler:  userService.updateUserEmail
-            },
-            {
-                method: "POST",
-                path: "/profile/image",
-                handler:  userService.uploadProfileImage,
-                options: {
-                    payload: {
-                        output: "stream",
-                        parse: true,
-                        multipart: true,
-                        maxBytes: 10 * 1024 * 1024,
-                        allow: "multipart/form-data"
-                    }
-                },
-            }
-        ]);
-    }
+  name: "userRoutes",
+  register: async function (server, options) {
+    server.route([
+      {
+        method: "GET",
+        path: "/profile",
+        handler: userService.getUserProfile,
+      },
+      {
+        method: "PUT",
+        path: "/profile",
+        handler: userService.updateUserProfile,
+      },
+      {
+        method: "PUT",
+        path: "/profile/email",
+        handler: userService.updateUserEmail,
+      },
+      {
+        method: "POST",
+        path: "/profile/image",
+        handler: userService.uploadProfileImage,
+        options: {
+          payload: {
+            output: "stream",
+            parse: true,
+            multipart: true,
+            maxBytes: 10 * 1024 * 1024,
+            allow: "multipart/form-data",
+          },
+        },
+      },
+    ]);
+  },
 };

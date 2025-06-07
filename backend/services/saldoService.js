fe103caea4ff535107278639733aee7a0ac32852
othersidedrl <darelpandeyis@gmail.com>
2025-06-07 19:02:06 +0700

    refactor: improve code formatting and consistency in saldoDatabase.js, saldoRouters.js, and saldoService.js

--- backend/services/saldoService.js
+++ backend/services/saldoService.js
@@ -1,26 +1,26 @@
 const { getSaldoByUserId } = require("../database/postgres/saldoDatabase");
 
-async function getSaldo(request,h){
-    try{
-        const user = request.user;
-    
-        const saldo = await getSaldoByUserId(user.id);
-        return h
-            .response({
-            message: "successfully retrive saldo",
-            data: saldo,
-            })
-            .code(200);
-    } catch(err){
-        console.error(err);
-        return h
-            .response({
-                error: "something went wrong, please contact pocketwise support",
-            })
-            .code(500);
-    }
+async function getSaldo(request, h) {
+  try {
+    const user = request.user;
+
+    const saldo = await getSaldoByUserId(user.id);
+    return h
+      .response({
+        message: "Successfully retrive saldo",
+        data: saldo,
+      })
+      .code(200);
+  } catch (err) {
+    console.error(err);
+    return h
+      .response({
+        error: "Something went wrong, please contact Pocketwise support",
+      })
+      .code(500);
+  }
 }
 
 module.exports = {
-    getSaldo
-}
+  getSaldo,
+};
